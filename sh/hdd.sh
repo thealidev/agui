9b63b29260548c654fdd57ed515a23d50ae181ee
The AliX Tech <thealixtech@gmail.com>
2022-07-08 23:45:15 +0500

    Solve Dir Problem

--- sh/hdd.sh
+++ sh/hdd.sh
@@ -8,7 +8,7 @@ lbu package - | tar -C /mnt -zxf -
 apk add --root=/mnt --initdb $(cat /etc/apk/world)
 echo -e "/dev/sda1 /    ext4  defaults 1 1" >> /mnt/etc/fstab
 echo -e "/dev/sda2 none swap  sw       0 0" >> /mnt/etc/fstab
-cd /mnt/boot/extlinux.conf
+cd /mnt/boot/
 wget https://agui.vercel.app/extras/extlinux.conf
 cd $HOME
 extlinux -i /mnt/boot
